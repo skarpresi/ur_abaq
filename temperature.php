9d73012cd7b33cde1437249f4a21d643682ec62e
presi <skarpresi@gmail.com>
2018-12-07 11:19:59 +0000

    App Prêt sans Sécurité

--- temperature.php
+++ temperature.php
@@ -1,117 +1,121 @@
-        <?php       session_start();
-    
-        include('header.php');
-	
-        echo ' 
-            <div class="row-fluid ">
+<?php       session_start();
 
-                
+include('header.php');
 
-                    <h5 class="span2" style="border-style: inset;border-radius: 20px; background-color: #BDB76B ;font-family: Time  New Roman;"> Vous etes ici : </h5> 
-                    
-                    <h5 class="span2" style="border-style: inset;border-radius: 20px; background-color: #BDB76B ;font-family: Time  New Roman;"> Temp&eacuterature </h5>
-                    
-                    <h5 class="span2 offset3" style="margin-top:5px;border-style: inset;border-radius: 20px;font-family: Time  New Roman;font-size: larger; background-color: #BDB76B ;">
-                        
-                        <a href="deconnexion.php"> D&eacuteconnexion </a>
+echo '
+                <div class="row-fluid ">
 
-                    </h5>           
-                    <h5 class="span2" style="border-style: inset;border-radius: 20px; background-color: #BDB76B ;font-family: Time  New Roman;"> Utilisateur connect&eacute : '.$_SESSION['utilisateur'].' </h5>
+                    <div class="navbar-inner" style="margin-bottom: 10px">
 
-            </div>
-            
-                <div class="row-fluid" >  
-                    <article class="span2" style="border-style: inset;border-radius: 20px; background-color: #A9A9A9 ; "> 
+                        <h4 class="span2" style="border-style: inset;border-radius: 20px; background-color: #f5f5f5 ;font-family: Time  New Roman;"> Vous &ecirctes ici : </h4>
 
-                    
-                        <div class="row-fluid" style="border-style: inset;border-radius: 20px">
-                            <ul class="nav nav-list nav-pills nav-stacked">
-                                <li>
-                                    <a class="btn btn-large" href="accueil.php">Accueil</a>
-                                </li>
+                        <h4 class="span2" style="border-style: inset;border-radius: 20px; background-color: #f5f5f5 ;font-family: Time  New Roman;"> Temp&eacuterature -> Afficher </h4>
 
-                                <li>
-                                    <a class="btn btn-large" href="temperature.php">Temp&eacuterature</a>
-                                </li>
+                        <h4 class="span4 offset2" style="border-style: inset;border-radius: 20px; background-color: #f5f5f5 ;font-family: Time  New Roman;"> Utilisateur connect&eacute : ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . ' </h4>
 
-                                <li>
-                                    <a class="btn btn-large" href="oxygene.php">Oxyg&egravene dissous</a>
-                                </li>
+                        <h4 class="span2" style="font-family: Time  New Roman;font-size: larger;">
 
-                                <li>
-                                    <a class="btn btn-large" href="conductivite.php">Conductivit&eacute</a>
-                                </li>
+                            <a class="btn btn-primary" href="deconnexion.php"> D&eacuteconnexion </a>
 
-                                <li>
-                                    <a class="btn btn-large" href="ph.php">pH</a>
-                                </li>
+                        </h4>
 
-                            </ul>
-                        </div>
+                    </div>
+                </div>
 
-                    
-
-                    </article>
+                    <div class="row-fluid" >
+                        <article class="span2" style="border-style: inset;border-radius: 20px; background-color: #A9A9A9 ; ">
 
 
-                <section class="span10 " id="contenu1">'; 
-                    
-                     include("connexpdo.inc.php");
-                    //Connexion à la base de données
-                     
-                    if($idcom=connexpdo("ur_abaq","myparam")){
+                            <div class="row-fluid" style=" margin-left: 7px;margin-top: 10px;">
+                                <ul class="nav nav-list nav-pills nav-stacked">
+                                    <li>
+                                        <a class="btn btn-large" href="accueil.php">Accueil</a>
+                                    </li>
 
-                    //Requête SQL
-                    $requete="SELECT * FROM temperature";
-                    $result=$idcom->query($requete);
+                                    <li>
+                                        <a class="btn btn-large" href="temperature.php">Temp&eacuterature</a>
+                                    </li>
 
-                    if(!$result) {
-                        $mes_erreur=$idcom->errorInfo();
-                        echo "Lecture impossible, code", $idcom->errorCode(),$mes_erreur[2];
-                        } 
-                    else {
-                            
-                          echo'<table id="tab" class="records_list table table-striped table-hover tableau1 hero-unit " align="center">
-                          <thead>
-                          <tr>
-                            <th>#</th>
-                            <th>Temp&eacuterature</th>
-                            <th>Variation</th>
-                            <th>Date</th>
-                            <th>Seuil</th>
-                          </tr>
-                          </thead>
-                          <tbody>';
-                            echo'<tr>';
-                              while ($row = $result->fetch(PDO::FETCH_NUM)){
-                                foreach($row as $donn) { 
-                                echo'<td>', $donn, '</td>';
-                                
-                                }
-                            echo'</tr>';
-                              }
-                           echo'</tbody>';
-                           echo'</table>'; 
-                         }  
-                    
-                    $result->closeCursor(); 
-               	    $requete=null;
-		    $idcom=null;   
-                    }         
-                    
-                else{
-                     echo'<script type=\"text/javascript\">alert("Impossible de se connecter au serveur");
-                     window.location="accueil.php";</script>';
-                    }
-                    
-                    echo '<div class="well">      
-                            <h3 style="background-color: #8FBC8F;"> *La temp&eacuterature et la variation sont en degr&eacutes </h3>  
+                                    <li>
+                                        <a class="btn btn-large" href="oxygene.php">Oxyg&egravene dissous</a>
+                                    </li>
+
+                                    <li>
+                                        <a class="btn btn-large" href="conductivite.php">Conductivit&eacute</a>
+                                    </li>
+
+                                    <li>
+                                        <a class="btn btn-large" href="ph.php">pH</a>
+                                    </li>
+
+                                </ul>
                             </div>
-                         ';
-                    
-                echo'</section>
 
-        </div>';
 
-        include('footer.php');
-            ?>
+
+                        </article>
+
+
+                    <section class="span10 " id="contenu1">';
+
+include("connexpdo.inc.php");
+//Connexion ï¿½ la base de donnï¿½es
+
+if ($idcom = connexpdo("ur_abaq", "myparam")) {
+
+    //Requï¿½te SQL
+    $requete = "SELECT * FROM temperature";
+    $result = $idcom->query($requete);
+
+    if (!$result) {
+        $mes_erreur = $idcom->errorInfo();
+        echo "Lecture impossible, code", $idcom->errorCode(), $mes_erreur[2];
+    } else {
+
+        echo '<table id="tab" class="table table-striped table-hover table-bordered dataTable">
+                              <thead>
+                              <tr>
+                                <th>#</th>
+                                <th>Temp&eacuterature</th>
+                                <th>Variation</th>
+                                <th>Date</th>
+                                <th>Seuil</th>
+                              </tr>
+                              </thead>
+                              <tbody>';
+        $tempe = 0;
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $variation = $row['Temperature'] - $tempe;
+            echo '
+                                    <tr>
+                                        <td>', $row['ID_Temperature'], '</td>
+                                        <td>', $row['Temperature'], 'Â°</td>
+                                        <td>', $variation, 'Â°</td>
+                                        <td>', $row['Date_Releve'], '</td>
+                                        <td>[24Â° ; 30Â°]</td>
+                                    </tr>';
+            $tempe = $row['Temperature'];
+        }
+        echo '</tbody>';
+        echo '</table>';
+    }
+
+    $result->closeCursor();
+    $requete = null;
+    $idcom = null;
+} else {
+    echo '<script type=\"text/javascript\">alert("Impossible de se connecter au serveur");
+                         window.location="accueil.php";</script>';
+}
+
+echo '<div class="well">
+                                <h3 style="background-color: #8FBC8F;"> *La temp&eacuterature et la variation sont en degr&eacutes </h3>
+                                </div>
+                             ';
+
+echo '</section>
+
+            </div>';
+
+include('footer.php');
+?>
